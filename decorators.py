e2a718bcbced5e05cd55c34c8b8bd998b9e870f4
Abhishek Gupta <abhishekmodi2011@gmail.com>
2017-09-26 15:19:34 +0530

    More in decorators

--- decorators.py
+++ decorators.py
@@ -1,3 +1,5 @@
+#Simple decorator working
+
 def simple_decorator(f):
     #This function will be used in place of orignal definition
     def wrapper():
@@ -8,9 +10,29 @@ def simple_decorator(f):
     return wrapper
 
 @simple_decorator
-def hello():
-    print("I am the hello function")
+def hello1():
+    print("I am the hello1 function")
+
+
+#Decorators with arguments
+def decorator_factory(enter_message, exit_message):
+    #This decorator is to be returned
+    def simple_decorator(f):
+        def wrapper():
+            print("Enter message: ", enter_message)
+            f()
+            print("Exit message: ", exit_message)
+        return wrapper
+
+    return simple_decorator
+
+@decorator_factory('Start', 'End')
+def hello2():
+    print('I am the hello2 function')
+
 
 if __name__ == '__main__':
-    print("Calling hello function now: ")
-    hello()
+    #print("Calling hello1 function now: ")
+    #hello1()
+    print("Calling the hello2 function")
+    hello2()
